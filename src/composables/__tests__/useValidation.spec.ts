90a4938b421dd755469ce77a752bfc3e852a4122
thekhairul <thekhairul.office@gmail.com>
2025-01-25 16:11:19 +0600

    fix useValidation tests

--- src/composables/__tests__/useValidation.spec.ts
+++ src/composables/__tests__/useValidation.spec.ts
@@ -6,28 +6,33 @@ import { ref } from 'vue';
 describe('useValidation', () => {
   it('should validate required field', () => {
     const value = ref('');
-    const { validate } = useValidation(value, [required]);
-
-    expect(validate()).toBe('This field is required');
+    const { error, validate } = useValidation(value, [required]);
+    validate();
+    expect(error.value).toBe('This field is required');
 
     value.value = 'value';
-    expect(validate()).toBe(null);
+    validate();
+    expect(error.value).toBe(null);
   });
 
   it('should validate range', () => {
     const value = ref('');
-    const { validate } = useValidation(value, [range(1, 10)]);
+    const { error, validate } = useValidation(value, [range(1, 10)]);
 
     value.value = '0';
-    expect(validate()).toBe('Value must be between 1 and 10');
+    validate();
+    expect(error.value).toBe('Value must be between 1 and 10');
 
     value.value = '5';
-    expect(validate()).toBe(null);
+    validate();
+    expect(error.value).toBe(null);
 
     value.value = '11';
-    expect(validate()).toBe('Value must be between 1 and 10');
+    validate();
+    expect(error.value).toBe('Value must be between 1 and 10');
 
     value.value = 'abc';
-    expect(validate()).toBe('Value must be between 1 and 10');
+    validate();
+    expect(error.value).toBe('Value must be between 1 and 10');
   });
 });
