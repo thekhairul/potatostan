5034de2756f3add969650078d7e926cca58c8019
thekhairul <thekhairul.office@gmail.com>
2025-01-25 21:02:59 +0600

    add sorting table component

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,8 @@
 <script setup lang="ts">
+import CountDown from '@/components/CountDown.vue';
+import SortingTable, { type TableRow } from '@/components/SortingTable.vue';
 import StartSortingBtn from '@/components/StartSortingBtn.vue';
+import { generatePeople } from '@/utils/training';
 import { ref } from 'vue';
 
 // types
@@ -15,10 +18,21 @@ const notice = ref(
   "Welcome to Potatostan. Start training by clicking the button above. Let's make Potatostan great again!",
 );
 const totalPeople = ref(0);
+const trainingData = ref<TableRow[]>([]);
 // methods
 const startTraining = (total: number) => {
   status.value = Status.Training;
   totalPeople.value = total;
+  trainingData.value = generatePeople(total);
+};
+const updateTrainingData = (data: TableRow[]) => {
+  trainingData.value = data;
+};
+const handleTimeout = () => {
+  status.value = Status.Fail;
+  totalPeople.value = 0;
+  trainingData.value = [];
+  notice.value = 'Time is up! You failed to sort the table in time. Try again!';
 };
 </script>
 
@@ -26,13 +40,23 @@ const startTraining = (total: number) => {
   <header class="container flex justify-between items-center gap-4 mx-auto mb-9">
     <h1 class="text-2xl font-bold">Sorting Training System</h1>
     <nav>
-      <start-sorting-btn @start="startTraining" />
+      <count-down
+        v-if="status === Status.Training"
+        :minutes="(5 * totalPeople) / 20"
+        @finish="handleTimeout"
+      />
+      <start-sorting-btn v-else @start="startTraining" />
     </nav>
   </header>
   <main class="container mx-auto">
     <section
       class="bg-white rounded-md shadow-default border border-grayscale-600" :class="{ 'h-96': status !== Status.Training }">
-      <div class="w-full h-full flex flex-col items-center justify-center">
+      <sorting-table
+        v-if="status === Status.Training"
+        :table-data="trainingData"
+        @update="updateTrainingData"
+      />
+      <div v-else class="w-full h-full flex flex-col items-center justify-center">
         <p class="text-lg max-w-96 text-center">{{ notice }}</p>
       </div>
     </section>
---
+++ src/components/SortingTable.vue
@@ -0,0 +1,76 @@
+<script setup lang="ts">
+import IconBars from '@/components/icons/IconBars.vue';
+import { computed, ref } from 'vue';
+import draggable from 'vuedraggable';
+export interface TableRow {
+  email: string;
+  potato: number;
+  name: string;
+}
+const props = defineProps<{
+  tableData: TableRow[];
+}>();
+const emit = defineEmits<{
+  (e: 'update', value: TableRow[]): void;
+}>();
+const model = computed({
+  get: () => props.tableData,
+  set: (value: TableRow[]) => {
+    emit('update', value);
+  },
+});
+const isDragging = ref(false);
+</script>
+
+<template>
+  <div class="flex justify-between items-center gap-4 pl-5 pr-8 py-6">
+    <p>Sort the table from most potatoes to least potatoes by dragging each row</p>
+    <p class="font-bold text-base text-grayscale-100">{{ tableData.length }} people in the list</p>
+  </div>
+  <div class="max-w-full overflow-x-auto mb-20">
+    <div class="w-full min-w-[800px]">
+      <!-- Table header -->
+      <div class="flex border-b border-t border-grayscale-600">
+        <div class="w-1/3 px-5 py-4 text-base text-grayscale-300 border-r border-grayscale-600">
+          Email
+        </div>
+        <div class="w-1/3 px-5 py-4 text-base text-grayscale-300">Potatoes</div>
+        <div class="w-1/3 px-5 py-4 text-base text-grayscale-300">Full name</div>
+      </div>
+      <!-- Table body -->
+      <draggable
+        v-model="model"
+        tag="div"
+        item-key="potato"
+        handle=".handle"
+        @start="isDragging = true"
+        @end="isDragging = false"
+        fallback-class="ps-table-draggable"
+        :force-fallback="true"
+        :scroll-sensitivity="200"
+      >
+        <template #item="{ element: row }">
+          <div
+            class="flex border-b border-grayscale-600 hover:bg-grayscale-800"
+            :class="{ 'select-none': isDragging }"
+          >
+            <div
+              class="w-1/3 flex gap-4 items-center px-5 py-4 text-base text-grayscale-100 border-r border-grayscale-600"
+            >
+              <icon-bars class="handle w-4 h-4 text-grayscale-300 hover:cursor-move" title="drag" />
+              {{ row.email }}
+            </div>
+            <div class="w-1/3 px-5 py-4 text-base text-grayscale-100">{{ row.potato }}</div>
+            <div class="w-1/3 px-5 py-4 text-base text-grayscale-100">{{ row.name }}</div>
+          </div>
+        </template>
+      </draggable>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.ps-table-draggable *{
+    border-right: 0;
+}
+</style>
